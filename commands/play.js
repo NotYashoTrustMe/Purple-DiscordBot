aba29f4f3b890c6ad2cdfe3040a27266c025b1d4
Yasho022 <wiredhack022@gmail.com>
2021-11-27 14:23:35 +0530

    Music Command Update

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders');	
+require('dotenv').config();
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,14 +7,25 @@ module.exports = {
 		.setDescription('Play a song from youtube')
         .addStringOption((option) => option
                 .setName('search').setDescription('Enter a song name/url(youtube) to play')
-                .setRequired(false)
+			.setRequired(false)
 		),
 	async execute(interaction) {
+		await interaction.deferReply();
 		var song = interaction.options.getString('search');
 		if (!song) {
 			// Play the songs in the queue
 		}
-		await interaction.deferReply();
+
+		const voiceChannel = interaction.member.voice.channel;
+		if (!voiceChannel) {
+			const musicChannel = process.env.MUSIC_CHANNEL;
+            const embed = new Embed()
+                .setColor(0xFF0000)
+                .setDescription(`You need to be in a voice channel, join the <#${musicChannel}>`)
+            return await interaction.reply({embeds: [embed]});
+        }
+
+		console.log(interaction.member.voice)
 		interaction.editReply(`Playing ${interaction.options.getString('search')}`);
 	}
 };
