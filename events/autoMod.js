ac3c1a07cb8797e82cbefe76af18a67326e3b704
Yasho022 <wiredhack022@gmail.com>
2021-12-15 17:02:51 +0530

    Added AUTOMOD

--- events/autoMod.js
+++ events/autoMod.js
@@ -8,10 +8,12 @@ module.exports = {
 		for (i of bannedWords) {
 			if (originalMessage.toLowerCase().includes(i)) {
 				message.delete();
-				const warning = message.channel.send(`**⚠️ ${message.author} has been warned for using a banned word**\n\nOriginal Message: \n\`\`\`${originalMessage}\`\`\`\n`);
+				const warning = message.channel.send(
+					`**⚠️ ${message.author} has been warned for using a banned word**\n\nOriginal Message: \n\`\`\`${originalMessage}\`\`\`\n`
+				);
 				setTimeout(() => {
-					warning.then (msg => msg.delete());
-				}, 3000);
+					warning.then((msg) => msg.delete());
+				}, 5000);
 			}
 		}
 	}
