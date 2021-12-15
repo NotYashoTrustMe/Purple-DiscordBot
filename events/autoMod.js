cb6ab7b3df66e27cd9ae1cc773884db695377956
Yasho022 <wiredhack022@gmail.com>
2021-12-15 17:02:10 +0530

    Added AutoMod

---
+++ events/autoMod.js
@@ -0,0 +1,18 @@
+const bannedWords = require('../config/bannedWords.json');
+module.exports = {
+	name    : 'messageCreate',
+
+	async execute(message) {
+		if (message.author.bot) return;
+		originalMessage = message.content;
+		for (i of bannedWords) {
+			if (originalMessage.toLowerCase().includes(i)) {
+				message.delete();
+				const warning = message.channel.send(`**⚠️ ${message.author} has been warned for using a banned word**\n\nOriginal Message: \n\`\`\`${originalMessage}\`\`\`\n`);
+				setTimeout(() => {
+					warning.then (msg => msg.delete());
+				}, 3000);
+			}
+		}
+	}
+};
