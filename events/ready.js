f0f2f777896340e88c4102ed820bdc409c0d77a7
Yasho022 <wiredhack022@gmail.com>
2021-12-15 19:37:17 +0530

    BugFix

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const GuildSettings = require('../models/GuildSettings.js');
 require('dotenv').config();
 
 module.exports = {
@@ -12,29 +13,30 @@ module.exports = {
 		}).setToken(process.env.TOKEN);
 
 		(async () => {
+			const guildSettings = await GuildSettings.findOne({});
+			const guildID = guildSettings.guildID;
 			try {
 				if (process.env.STAGE == 'production') {
-					await rest.put(Routes.applicationCommands(client.user.id), { body: commands });
-					console.log('\u001B[36mRegistered Commands \u001B[32mGlobally ✔️\u001B[0m');
+					await rest.put(Routes.applicationCommands(client.user.id, guildID), { body: commands });
+					console.log(`\u001B[36m` + `[✓] Registered Commands ` + `\u001B[32m` + `Globally` + `\u001B[0m`);
 				}
 				else {
-					//? If you are running for the first time or adding your own command, change it to this to this to register the command:
-					/*
-					await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID), {body: commands});
-					*/
-					await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID), {
-						body : commands
+					/* If you are running for the first time or adding your own command, change it to this to this to register the command:
+					* await rest.put(Routes.applicationGuildCommands(client.user.id, guildID), {body: commands});
+				   */
+					await rest.put(Routes.applicationGuildCommands(client.user.id, guildID), {
+						body : []
 					});
-					console.log('\u001B[36mRegistered Commands\u001B[33m Locally ✔️\u001B[0m');
+					console.log(`\u001B[36m` + `[✓] Registered Commands` + `\u001B[33m ` + `Locally` + `\u001B[0m`);
 				}
 			} catch (err) {
 				if (err) {
-					console.log(`\u001B[31m${err}\u001B[0m`);
+					console.log(`\u001B[31m` + `[x] ${err}` + `\u001B[0m`);
 				}
 			}
 		})();
 
-		console.log(`\u001B[36mPurple is Online 🟢\u001B[0m`);
+		console.log(`\u001B[36m` + `[✓] Purple is Online` + `\u001B[0m`);
 		client.user.setActivity(`Commands ▲ /help`);
 	}
 };
